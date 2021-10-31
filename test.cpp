34dd7c8d5169092b14fb2b6b8b8bb64ac5d7f7f8
folkert van heusden <mail@vanheusden.com>
2021-10-31 10:35:55 +0100

    GTP

--- test.cpp
+++ test.cpp
@@ -2,37 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "proc.h"
+#include "gtp.h"
 
 int main(int argc, char *argv[])
 {
-	TextProgram *tp = new TextProgram("/usr/games/gnugo --mode gtp --level 0", "/tmp");
+	GtpEngine *ge = new GtpEngine("/usr/games/gnugo --mode gtp --level 0", "/tmp");
 
-	for(int i=0; i<2; i++) {
-		if (i == 0)
-			assert(tp->write("genmove b"));
-		else
-			assert(tp->write("genmove w"));
+	ge->boardsize(9);
+	ge->clearboard();
 
-		std::optional<std::string> rc1 = tp->read(1000);
-		assert(rc1.has_value());
+	auto rc = ge->getname();
+	assert(rc.has_value());
+	printf("name: %s\n", rc.value().c_str());
 
-		printf("%s\n", rc1.value().c_str());
-	}
+	bool rc2 = ge->play(C_BLACK, "a1");
+	assert(rc2);
 
-	assert(tp->write("final_score"));
+	auto rc3 = ge->genmove(C_WHITE, { });
+	assert(rc3.has_value());
+	printf("move: %s\n", rc3.value().c_str());
 
-	for(;;) {
-		std::optional<std::string> rc2 = tp->read(1100);
-		assert(rc2.has_value());
-
-		if (rc2.value().empty() == false) {
-			printf("%s\n", rc2.value().c_str());
-			break;
-		}
-	}
-
-	delete tp;
+	delete ge;
 
 	printf("---\n");
 
